c7c574c2714d9823ed1a257b35bd53cdd0bca3bd
anuswe <anu.nimmi94@gmail.com>
2014-08-09 16:55:47 +0530

    bug creation done

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ SimpleBugTracker::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+   resources :bugs
+     
+   
 
   # Sample resource route with options:
   #   resources :products do
